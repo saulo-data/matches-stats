060cbc0ec34b099cc25e3f3b103ae7bfe6e7bb8a
Saulo Faria <saulo82faria@gmail.com>
2025-09-09 20:30:12 -0300

    Update match_analysis.py

--- match_analysis.py
+++ match_analysis.py
@@ -60,7 +60,6 @@ with st.sidebar:
     st.write("This Web App was designed in order to get smart plots of a plethora of matches (current season). If you want to have more details about these matches, other stats, other seasons or believe I can help you in your project, send an email to saulo.foot@gmail.com. I'm always open to work.")
 
     st.subheader("My links (pt-br)")
-    st.link_button("Aposta Consciente", "https://go.hotmart.com/Q98778179P?dp=1", use_container_width=True)
     st.link_button("Instagram", "https://www.instagram.com/saulo.foot/", use_container_width=True)
     st.link_button("X", "https://x.com/fariasaulo_", use_container_width=True)
     st.link_button("Youtube", "https://www.youtube.com/channel/UCkSw2eyetrr8TByFis0Uyug", use_container_width=True)
